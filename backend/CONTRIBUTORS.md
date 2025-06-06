265ad3b7ea78a757878949d73bfbd9ac1417ebb4
Dobricean Ioan Dorian <50819975+dobri1408@users.noreply.github.com>
2025-06-06 22:04:22 +0300

    volto luna sites

---
+++ backend/CONTRIBUTORS.md
@@ -0,0 +1,3 @@
+# Contributors
+
+- LunaSites [dobriceanionut@luna-sites.com]
---
+++ frontend/volto.config.js
@@ -0,0 +1,21 @@
+const addons = [
+  '@plonegovbr/volto-social-media',
+  '@eeacms/volto-accordion-block',
+  '@kitconcept/volto-banner-block',
+  '@kitconcept/volto-button-block',
+  '@kitconcept/volto-carousel-block',
+  '@kitconcept/volto-heading-block',
+  '@kitconcept/volto-highlight-block',
+  '@kitconcept/volto-introduction-block',
+  '@kitconcept/volto-logos-block',
+  '@kitconcept/volto-separator-block',
+  '@kitconcept/volto-slider-block',
+  '@kitconcept/volto-light-theme',
+
+  'volto-lunasites',
+];
+const theme = '@kitconcept/volto-light-theme';
+module.exports = {
+  addons,
+  theme,
+};
