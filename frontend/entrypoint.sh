ab39c8e803eacd89624e790dca2fbe59918f1f70
Dobricean Ioan Dorian <50819975+dobri1408@users.noreply.github.com>
2025-07-29 12:05:20 +0300

    test

---
+++ frontend/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -Ex
+
+function apply_rebuild {
+  mkdir -p /app/src/addons
+  gosu node pnpm develop
+  gosu node pnpm install
+  gosu node pnpm build
+}
+
+# Should we re-build
+test -n "$REBUILD" && apply_rebuild
+
+if [[ "$1" == "pnpm"* ]]; then
+  echo "Starting Volto"
+  exec gosu node "$@"
+fi
+
+exec "$@"
